a3b5e68a815c36f9542abbc28807da7172f4e58d
caio7250 <caio.oliveira7250@gmail.com>
2024-06-13 13:24:04 -0300

    Ler nome do arquivo

--- src/leitura_arquivos.c
+++ src/leitura_arquivos.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define PREFIXO "../data/" //Caso esteja executando no vscode utilize "../../data/" pois o vscode executa o terminal na pasta output.
 
 int main () {
 
   FILE *file; //Ponteiro do arquivo, deixei o nome genérico pois precisaremos apenas de um por enquanto.
+  char nomeArquivo[100]; // Variável para armazenar o nome do arquivo
+  char caminhoArquivo[150]; // Buffer pra armazenar o caminho do arquivo concatenado
 
-  file = fopen("a.txt", "r"); //Função para abertura do arquivo em modo de leitura (read) -- Estudar pra conseguir fazer o usuário inserir o nome do arquivo e ele conseguir ser inserido no GENERICO (não é variável, é só pra eu saber depois mesmo)
+  printf("Digite o nome do arquivo: ");
+  scanf("%s", nomeArquivo); //Ler e guardar o nome do arquivo a ser aberto
+
+  snprintf(caminhoArquivo, 150, "%s%s", PREFIXO, nomeArquivo);
+
+  file = fopen(caminhoArquivo, "r"); //Função para abertura do arquivo em modo de leitura (read) -- Estudar pra conseguir fazer o usuário inserir o nome do arquivo e ele conseguir ser inserido no GENERICO (não é variável, é só pra eu saber depois mesmo)
 
   if (file == NULL)
   {
     printf("O arquivo informado não existe.");
-  } // Verifica se o arquivo digitado existe, se não existir exibe a mensagem (ver se é pra terminar o programa depois com o murilo ou refazer a função de inserir texto com um while)
+    return 0;
+  } // Verifica se o arquivo digitado existe, se não existir exibe a mensagem
   // Caso contrário continuará o código normalmente
 
   int identificacao_carga, numero_protocolo, numero_amostras, tipo_produto;
   float peso_bruto; // Declaração das variáveis da primeira linha dos arquivos
 
   fscanf(file, "%d %d %f %d %d", &identificacao_carga, &numero_protocolo, &peso_bruto, &numero_amostras, &tipo_produto); // Lê os arquivos da primeira linha e armazena nas variiáveis
+
+  // printf("idcarga = %d, numero_protocolo = %d, pesoBruto = %f, numero_amostras = %d, tipo produto = %d\n", identificacao_carga, numero_protocolo, peso_bruto, numero_amostras, tipo_produto); //Verificar se armazenou certinho (DESCOMENTE CASO QUEIRA VER)
 
   char conteudoTemporario [100]; //vetor para armazenarmos as strings temporárias lidas pelo fgets
 
@@ -33,6 +44,8 @@ int main () {
   {
     //Aqui cada vez que o laço roda ele armazena os dados de cada amostra nas variáveis temporárias, dentro desse while faremos os cálculos com cada variável
     sscanf(conteudoTemporario, "%d %f %d %f", &identificacaoAmostra, &pesoBrutoAmostra, &pesoImpurezas, &grauUmidade); 
+
+    // printf("%d %f %d %f\n", identificacaoAmostra, pesoBrutoAmostra, pesoImpurezas, grauUmidade); CASO QUEIRA ANALISAR O QUE ESTA SENDO LIDO SÓ DESCOMENTAR ISSO AQUI
     
   }
 
